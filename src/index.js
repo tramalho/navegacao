44deea2741348163032d4a873d4a69584c48eb27
tramalho <thiagofacul@gmail.com>
2025-01-15 07:41:08 -0300

    Estrutura do Projeto

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+const domNode = document.getElementById("root");
+const root = ReactDOM.createRoot(domNode);
+root.render(<h1>App</h1>);
